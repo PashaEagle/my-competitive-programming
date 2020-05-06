569231b0a93b5b46933e6662094f447d9613cc22
Pasha Kolesnyk <kolpal17@gmail.com>
2020-05-06 17:51:06 +0300

    New leetcode May challenge problem (6 day, #169)

---
+++ leetcode/May Coding Challenge/0169_MajorityElement.java
@@ -0,0 +1,16 @@
+// Wed, 6th May, 2020 - Leetcode may coding challenge, day #6
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
